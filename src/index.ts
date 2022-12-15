fc8b5da86b127f3bfbe31e7fa12e88809f82974a
Nick <nicolastiennguyen@gmail.com>
2022-12-15 13:09:03 -0800

    initial commit

--- src/index.ts
+++
@@ -1,8 +0,0 @@
-// import { v4 as uuidV4 } from 'uuid';
-
-// console.log(uuidV4())
-
-const list = document.querySelector<HTMLUListElement>("#list")
-const form = document.querySelector<HTMLFormElement>("#new-task-form")
-// another option: const form = document.getElementById("#new-task-form") as HTMLFormElement | null
-const input = document.querySelector<HTMLInputElement>("#new-task-title")
